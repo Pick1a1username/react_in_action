c70bd2e8df8d56b88a1658a0bae7bd723bc0a2bd
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2019-10-20 17:13:49 +0900

    20191020_1

---
+++ chapter4/listing-4.2./src/index.js
@@ -0,0 +1,85 @@
+import Proptypes from 'prop-types';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class ChildComponent extends Component {
+    static propTypes = {
+        name: Proptypes.string
+    };
+
+    static defaultProps = (function() {
+        console.log('ChildComponent : defaultProps')
+        return {};
+    })();
+
+    constructor(props) {
+        super(props);
+        console.log('ChildComponent: state');
+        this.state = {
+            name: 'Mark',
+        };
+    }
+
+    componentWillMount() {
+        console.log('ChildComponent: componentWillMount')
+    }
+
+    componentDidMount() {
+        console.log('ChildComponent: componentDidMount');
+    }
+
+    render() {
+        if (this.state.oops) {
+            throw new Error('Something went wrong');
+        }
+
+        console.log('ChildComponent: render');
+
+        return [
+            <div key="name">Name: {this.props.name}</div>
+        ];
+    }
+};
+
+class ParentComponent extends Component {
+    static defaultProps = (function() {
+        console.log('ParentComponent: defaultProps');
+        return {
+            true: false
+        };
+    })();
+
+    constructor(props) {
+        super(props);
+        console.log('ParentComponent: state');
+        this.state = { name: '' };
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    componentWillMount() {
+        console.log('ParentComponent: componentWillMount');
+    }
+
+    componentDidMount() {
+        console.log('ParentComponent: componentDidMount');
+    }
+    
+    onInputChange(e) {
+        const text = e.target.value;
+        this.setState({ text: text });
+    };
+
+    render() {
+        console.log('ParentComponent: render');
+        return [
+            <h2 key="h2">Learn about rendering and lifecycle methods!</h2>,
+            <input key="input" value={this.state.text} onChange={this.onInputChange} />,
+            <ChildComponent key="ChildComponent" name={this.state.text} />
+        ];
+    };
+};
+
+render(
+    <ParentComponent />,
+    document.getElementById('container')
+);
